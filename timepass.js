f01f4a4642a79a46fa29662c990f83fdbd7139da
Anushlinux <anushrutpandit@gmail.com>
2024-05-25 20:51:16 +0530

    new

--- timepass.js
+++ timepass.js
@@ -1,8 +1,12 @@
-function Book(title, author, pages, read){
-    this.title = title;
+function Book(name, author, pages, read) {
+    this.name = name;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function(){
-        return `${title} by ${author}, ${pages} pages, ${read}`;
+    this.info = function() {
+        return `${name} by ${author}, ${pages}, ${read ? "true" : "false"}`
     }
+}
+const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
+Object.getPrototypeOf(book1);
+console.log(book1.info());
