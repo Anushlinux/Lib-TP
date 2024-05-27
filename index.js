e249830826e9fe5e2da717290991015307e0b7c5
Anushlinux <anushrutpandit@gmail.com>
2024-05-28 02:49:30 +0530

    Fk it we ball

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-// const askName = prompt("Whats your name?");
-
-
-// function Book(name, author, pages, read) {
-//   this.name = name;
-//   this.author = author;
-//   this.pages = pages;
-//   this.read = read;
-//   this.info = function () {
-//     return `${name} by ${author}, ${pages}, ${read ? "true" : "false"}`;
-//   };
-// }
-
-// function addbooktolib() {
-
-// }
+const askName = prompt("Whats your name?");
 
 var video = document.getElementById("bdVid");
 
 var btn = document.getElementById("myBtn");
 
-function myFunction() {
-  if (video.paused) {
-    video.play();
-    btn.innerHTML = "NewBook";
-  } else {
-    video.pause();
-    btn.innerHTML = "NewBook";
-  }
+// function myFunction() {
+//   if (video.paused) {
+//     video.play();
+//     btn.innerHTML = "NewBook";
+//   } else {
+//     video.pause();
+//     btn.innerHTML = "NewBook";
+//   }
+// }
+
+const myLibrary = [];
+
+function Book() {
+  this.name = "";
+  this.author = "";
+  this.pages = "";
+  this.read = "";
 }
+
+function addBookToLibrary() {
+
+}
+
